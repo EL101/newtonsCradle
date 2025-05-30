41c5b617ca958e3f911dac19cfe79d122fa9a68f
Ethan Liu <eliu3152@gmail.com>
2025-05-29 21:04:46 -0400

    Fixed collisions with 3 balls, created program rate slider

--- code.py
+++ code.py
@@ -7,7 +7,7 @@ initial_angle=-pi/3
 axis_pos=3
 ball_radius=0.3
 
-num_balls=2
+num_balls=3
 num_starting_balls=1
 #def setInitialAngle(x):
 #    global initial_angle
@@ -27,11 +27,12 @@ balls=[]
 rods=[]
 pos_curves=[]
 vel_curves=[]
-balls.append(sphere(pos=vec(L * sin(initial_angle),axis_pos-L * cos(initial_angle),0), radius=ball_radius, color=color.cyan))
-rods.append(cylinder(pos=balls[0].pos, axis=vec(0,axis_pos,0)-balls[0].pos, radius=0.03, color=color.red))
-for i in range(1,num_balls):
+for i in range(num_starting_balls):
+    balls.append(sphere(pos=vec(L * sin(initial_angle)+2*i*ball_radius,axis_pos-L * cos(initial_angle),0), radius=ball_radius, color=color.cyan))
+    rods.append(cylinder(pos=balls[i].pos, axis=vec(0,axis_pos,0)-balls[i].pos, radius=0.03, color=color.red))
+for i in range(num_starting_balls,num_balls):
     balls.append(sphere(pos=vec(2*i*ball_radius,axis_pos-L,0), radius=ball_radius, color=color.cyan))
-    rods.append(cylinder(pos=balls[i].pos, axis=vec(2*i*ball_radius,axis_pos,0)-balls[i].pos, radius=0.03, color=color.red))
+    rods.append(cylinder(pos=balls[i].pos, axis=vec(0,L,0), radius=0.03, color=color.red))
 
 g = 9.81
 for i in range(num_balls):
@@ -42,35 +43,51 @@ for i in range(num_balls):
     pos_curves.append(gcurve(graph=pos_graph, color=curr_color, label="Ball "+str(i)))
     vel_curves.append(gcurve(graph=vel_graph, color=curr_color, label="Ball "+str(i)))
     
-balls[0].theta=initial_angle
+for i in range(num_starting_balls):
+    balls[i].theta=initial_angle
 t = 0
 dt = 1/200
-program_rate = 100
+program_rate = 500
 
+def setRt(x):
+    global program_rate
+    program_rate = x.value
 
 def collide(i, j):
-    balls[j].acc=balls[i].acc
     balls[j].vel=balls[i].vel
-    balls[i].acc=0
     balls[i].vel=0
+    balls[i].theta=0
+
+slider_title = wtext(text = "Set Rate:")
+rateSlider = slider(bind = setRt, min=10, max=2000, value=500, step=10)
 while (True):
     rate(program_rate)
     for i in range(num_balls):
         ball=balls[i]
+        if ball.vel==0 and ball.theta==0:
+            continue
+        for j in range(i+1, num_balls):
+            if ball.vel.mag!=0:
+                if (ball.pos.x>balls[j].pos.x-2*ball_radius):
+                    collide(i,j)
+#                    print(balls[i].pos.x)
+                    break
+        for j in range(i-1, -1, -1):
+            if ball.vel.mag!=0:
+                if (ball.pos.x<balls[j].pos.x+2*ball_radius):
+#                    print(i,j,ball.pos.x,balls[j].pos.x)
+                    collide(i,j)
+                    print(i,j,ball.pos.x,balls[j].pos.x)
+                    break
         ball.acc=-g * sin(ball.theta)/L
         ball.vel+=ball.acc*dt
         ball.theta+=ball.vel*dt
         ball.pos=vec(L * sin(ball.theta)+2*i*ball_radius,axis_pos-L * cos(ball.theta),0)
         rods[i].pos=ball.pos
         rods[i].axis=vec(2*i*ball_radius,axis_pos,0)-ball.pos
-        for j in range(0, num_balls):
-            if ball.vel.mag!=0:
-                if (j>i and ball.pos.x>=balls[j].pos.x-2*ball_radius):
-                    collide(i,j)
-                else if (j<i and ball.pos.x<=balls[j].pos.x+2*ball_radius):
-                    collide(i,j)
 #    print(ball.acc + " " + ball.vel + " " + theta)
-        pos_curves[i].plot(t, ball.pos.y)
-        vel_curves[i].plot(t, ball.vel)
+    for i in range(num_balls):
+        pos_curves[i].plot(t, balls[i].pos.y)
+        vel_curves[i].plot(t, balls[i].vel)
     t+=dt
     
