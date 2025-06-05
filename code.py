dc67029bc8c2bcb1e0b4c16c95eec17ab1c8f7b9
Ethan Liu <eliu3152@gmail.com>
2025-06-05 15:16:52 -0400

    Made buttons for choosing between different modes of input.

--- code.py
+++ code.py
@@ -1,5 +1,6 @@
 Web VPython 3.2
 scene = canvas(width=1000, height=450)
+scene.userzoom = False
 
 program_rate = 1000
 
@@ -42,27 +43,48 @@ def setNumBallsStarting(x):
     
 def start():
     set_initial_values.disabled=True
-#    initial_angle_slider.delete()
     num_balls_slider.delete()
-#    num_starting_balls_slider.delete()
-#    initial_angle_slider_caption.delete()
     num_balls_slider_caption.delete()
-#    num_starting_balls_slider_caption.delete()
-       
-#initial_angle_slider_caption = wtext(text = "Set Initial Angle:")
-#initial_angle_slider = slider( bind=setInitialAngle, min=0, max=90, value=60, step=1)
-#initial_angle_text = wtext(text = "Angle: "+str(initial_angle_slider.value))
-#scene.append_to_caption('\n\n')
+    if (mode==2):
+        num_starting_balls_slider.delete()
+        initial_angle_slider_caption.delete()
+        initial_angle_slider.delete()
+        num_starting_balls_slider_caption.delete()
+
+mode=0
+def click_drag():
+    global mode
+    mode=1
+def manual():
+    global mode
+    mode=2
+
+input_text = text(text='Choose Mode of Input', align='center', color=color.red, visible=True)
+click_drag_button = button(bind=click_drag, text="Click and Drag")
+manual_button = button(bind=manual, text="Manual Input")
+
+while mode==0:
+    rate(program_rate)
+
+input_text.visible=False
+click_drag_button.delete()
+manual_button.delete()
 
 num_balls_slider_caption = wtext(text = "Set Total # of Balls:")
-num_balls_slider = slider(bind=setNumBalls, min=1, max=10, value=1, step=1)
+num_balls_slider = slider(bind=setNumBalls, min=1, max=10, value=num_balls, step=1)
 num_balls_text = wtext(text = "# of Balls: "+str(num_balls_slider.value))
 scene.append_to_caption('\n\n')
 
-#num_starting_balls_slider_caption = wtext(text = "Set Total # of Elevated Balls:")
-#num_starting_balls_slider = slider(bind=setNumBallsStarting, min=1, max=10, value=1, step=1)
-#num_starting_balls_text = wtext(text = "# of Elevated Balls: "+str(num_starting_balls_slider.value))
-#scene.append_to_caption('\n\n')
+if mode==2:
+    initial_angle_slider_caption = wtext(text = "Set Initial Angle:")
+    initial_angle_slider = slider( bind=setInitialAngle, min=0, max=90, value=initial_angle, step=1)
+    initial_angle_text = wtext(text = "Angle: "+str(initial_angle_slider.value))
+    scene.append_to_caption('\n\n')
+    
+    num_starting_balls_slider_caption = wtext(text = "Set Total # of Elevated Balls:")
+    num_starting_balls_slider = slider(bind=setNumBallsStarting, min=1, max=10, value=num_starting_balls, step=1)
+    num_starting_balls_text = wtext(text = "# of Elevated Balls: "+str(num_starting_balls_slider.value))
+    scene.append_to_caption('\n\n')
 
 set_initial_values = button(bind=start, text="Start", disabled=False)
 scene.append_to_caption('\n\n')
@@ -93,8 +115,6 @@ def on_click(ball, mouse):
     dist=sqrt((mouse.x-ball.pos.x)*(mouse.x-ball.pos.x)+(mouse.y-ball.pos.y)*(mouse.y-ball.pos.y)+(mouse.z-ball.pos.z)*(mouse.z-ball.pos.z))
     return dist<=ball_radius
 
-click_drag_text = text(text='Click and Drag', pos=vec(-4,3,0), color=color.red, visible=True)
-
 drag=False
 def start_drag():
     global drag
@@ -103,71 +123,56 @@ def stop_drag():
     global drag
     drag=False
 
-scene.bind('mousedown', start_drag)
-scene.bind('mouseup', stop_drag)
-
-mouse_down = scene.waitfor('mousedown')
-
-starting_ball=-1
-while (starting_ball==-1):
-    rate(program_rate)
-    for i in range(0,num_balls):
-        if (on_click(balls[i], mouse_down.pos)):
-            starting_ball=i
-            break
-
-while drag:
-    rate(100)
-#    print(scene.mouse.pos.x, scene.mouse.pos.y)
-    initial_angle=asin(max(-1, min(1, (scene.mouse.pos.x-mouse_down.pos.x)/L)))
-    if (initial_angle<0):
-        num_starting_balls=starting_ball+1
-        for i in range(num_starting_balls):
-            balls[i].pos=pos=vec(L * sin(initial_angle)+2*i*ball_radius+offset,axis_pos-L * cos(initial_angle),0)
-            rods[i].pos=balls[i].pos
-            rods[i].axis=vec(2*i*ball_radius+offset,axis_pos,0)-balls[i].pos
-            balls[i].theta=initial_angle
-        for i in range(starting_ball, num_balls):
-            balls[i].pos=vec(2*i*ball_radius+offset, axis_pos-L,0)
-            rods[i].pos=balls[i].pos
-            rods[i].axis=vec(2*i*ball_radius+offset,axis_pos,0)-balls[i].pos
-            balls[i].theta=0
-    else:
-        num_starting_balls=num_balls-starting_ball
-        for i in range(starting_ball, num_balls):
-            balls[i].pos=vec(L * sin(initial_angle)+2*i*ball_radius+offset,axis_pos-L * cos(initial_angle),0)
-            rods[i].pos=balls[i].pos
-            rods[i].axis=vec(2*i*ball_radius+offset,axis_pos,0)-balls[i].pos
-            balls[i].theta=initial_angle
-        for i in range(0, starting_ball):
-            balls[i].pos=vec(2*i*ball_radius+offset, axis_pos-L,0)
-            rods[i].pos=balls[i].pos
-            rods[i].axis=vec(2*i*ball_radius+offset,axis_pos,0)-balls[i].pos
-            balls[i].theta=0
-#    
-#mouse_up = scene.waitfor('mouseup')
-click_drag_text.visible=False
-
-#initial_angle=atan((mouse_up.pos.x-balls[starting_ball].pos.x)/(mouse_up.pos.y-balls[starting_ball].pos.y))
-#
-#if (initial_angle<0):
-#    num_starting_balls=starting_ball+1
-#    for i in range(num_starting_balls):
-#        balls[i].pos=pos=vec(L * sin(initial_angle)+2*i*ball_radius+offset,axis_pos-L * cos(initial_angle),0)
-#        rods[i].pos=balls[i].pos
-#        rods[i].axis=vec(2*i*ball_radius+offset,axis_pos,0)-balls[i].pos
-#        balls[i].theta=initial_angle
-#else:
-#    num_starting_balls=num_balls-starting_ball
-#    for i in range(starting_ball, num_balls):
-#        balls[i].pos=pos=vec(L * sin(initial_angle)+2*i*ball_radius+offset,axis_pos-L * cos(initial_angle),0)
-#        rods[i].pos=balls[i].pos
-#        rods[i].axis=vec(2*i*ball_radius+offset,axis_pos,0)-balls[i].pos
-#        balls[i].theta=initial_angle
-
-#while True:
-#    rate(program_rate)
-
+if mode==1:
+    scene.bind('mousedown', start_drag)
+    scene.bind('mouseup', stop_drag)
+    click_drag_text = text(text='Click and Drag', pos=vec(0,3,0), align='center', color=color.red, visible=True)
+    mouse_down = scene.waitfor('mousedown')
+    
+    starting_ball=-1
+    while (starting_ball==-1):
+        rate(program_rate)
+        for i in range(0,num_balls):
+            if (on_click(balls[i], mouse_down.pos)):
+                starting_ball=i
+                break
+    
+    while drag:
+        rate(100)
+    #    print(scene.mouse.pos.x, scene.mouse.pos.y)
+        initial_angle=asin(max(-1, min(1, (scene.mouse.pos.x-mouse_down.pos.x)/L)))
+        if (initial_angle<0):
+            num_starting_balls=starting_ball+1
+            for i in range(num_starting_balls):
+                balls[i].pos=pos=vec(L * sin(initial_angle)+2*i*ball_radius+offset,axis_pos-L * cos(initial_angle),0)
+                rods[i].pos=balls[i].pos
+                rods[i].axis=vec(2*i*ball_radius+offset,axis_pos,0)-balls[i].pos
+                balls[i].theta=initial_angle
+            for i in range(starting_ball, num_balls):
+                balls[i].pos=vec(2*i*ball_radius+offset, axis_pos-L,0)
+                rods[i].pos=balls[i].pos
+                rods[i].axis=vec(2*i*ball_radius+offset,axis_pos,0)-balls[i].pos
+                balls[i].theta=0
+        else:
+            num_starting_balls=num_balls-starting_ball
+            for i in range(starting_ball, num_balls):
+                balls[i].pos=vec(L * sin(initial_angle)+2*i*ball_radius+offset,axis_pos-L * cos(initial_angle),0)
+                rods[i].pos=balls[i].pos
+                rods[i].axis=vec(2*i*ball_radius+offset,axis_pos,0)-balls[i].pos
+                balls[i].theta=initial_angle
+            for i in range(0, starting_ball):
+                balls[i].pos=vec(2*i*ball_radius+offset, axis_pos-L,0)
+                rods[i].pos=balls[i].pos
+                rods[i].axis=vec(2*i*ball_radius+offset,axis_pos,0)-balls[i].pos
+                balls[i].theta=0
+    
+    click_drag_text.visible=False
+elif mode==2:
+    for i in range(0, num_starting_balls):
+        balls[i].pos=pos=vec(L * sin(initial_angle)+2*i*ball_radius+offset,axis_pos-L * cos(initial_angle),0)
+        rods[i].pos=balls[i].pos
+        rods[i].axis=vec(2*i*ball_radius+offset,axis_pos,0)-balls[i].pos
+        balls[i].theta=initial_angle
 g = 9.81
 t = 0
 dt = 1/2000
@@ -197,6 +202,7 @@ pause_button=button(bind=pause, text='Pause', background=color.red)
 
 display_graph=True
 graph_spacing=10
+
 def change_display_graph():
     global display_graph
     display_graph=!display_graph
@@ -206,7 +212,10 @@ def change_display_graph():
     else:
         display_graph_button.background=color.red
         display_graph_button.text='Undisplay'
+
 display_graph_button=button(bind=change_display_graph, text='Undisplay', background=color.red)
+
+print(num_balls, num_starting_balls, initial_angle)
 while (True):
     rate(program_rate)
     if !run:
@@ -216,10 +225,10 @@ while (True):
         if balls[j].theta==0 and balls[j].vel==0:
             moving-=1
     if (moving!=num_starting_balls):
-        print(moving)
-        for ball in balls:
-            print(ball.prev_theta, ball.prev_vel, ball.theta, ball.vel)
-        run=False
+#        print(moving)
+#        for ball in balls:
+#            print(ball.prev_theta, ball.prev_vel, ball.theta, ball.vel)
+#        run=False
         break
     for i in range(num_balls):  
         ball=balls[i]
