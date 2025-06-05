3a602a78230d13be75be78fb64ff8b3a40e63735
Ethan Liu <eliu3152@gmail.com>
2025-06-05 11:26:22 -0400

    Added click & drag feature. Balls follow cursor in click drag.

--- code.py
+++ code.py
@@ -1,5 +1,5 @@
 Web VPython 3.2
-scene = canvas(width=1000, height=500)
+scene = canvas(width=1000, height=450)
 
 program_rate = 1000
 
@@ -20,9 +20,9 @@ L=3
 axis_pos=3
 ball_radius=0.3
 
-initial_angle=-pi/3
+initial_angle=0
 num_balls=1
-num_starting_balls=1
+num_starting_balls=0
 
 def setInitialAngle(x):
     global initial_angle
@@ -42,33 +42,34 @@ def setNumBallsStarting(x):
     
 def start():
     set_initial_values.disabled=True
-    initial_angle_slider.delete()
+#    initial_angle_slider.delete()
     num_balls_slider.delete()
-    num_starting_balls_slider.delete()
-    initial_angle_slider_caption.delete()
+#    num_starting_balls_slider.delete()
+#    initial_angle_slider_caption.delete()
     num_balls_slider_caption.delete()
-    num_starting_balls_slider_caption.delete()
+#    num_starting_balls_slider_caption.delete()
        
-initial_angle_slider_caption = wtext(text = "Set Initial Angle:")
-initial_angle_slider = slider( bind=setInitialAngle, min=0, max=90, value=60, step=1)
-initial_angle_text = wtext(text = "Angle: "+str(initial_angle_slider.value))
-scene.append_to_caption('\n\n')
+#initial_angle_slider_caption = wtext(text = "Set Initial Angle:")
+#initial_angle_slider = slider( bind=setInitialAngle, min=0, max=90, value=60, step=1)
+#initial_angle_text = wtext(text = "Angle: "+str(initial_angle_slider.value))
+#scene.append_to_caption('\n\n')
 
 num_balls_slider_caption = wtext(text = "Set Total # of Balls:")
 num_balls_slider = slider(bind=setNumBalls, min=1, max=10, value=1, step=1)
 num_balls_text = wtext(text = "# of Balls: "+str(num_balls_slider.value))
 scene.append_to_caption('\n\n')
 
-num_starting_balls_slider_caption = wtext(text = "Set Total # of Elevated Balls:")
-num_starting_balls_slider = slider(bind=setNumBallsStarting, min=1, max=10, value=1, step=1)
-num_starting_balls_text = wtext(text = "# of Elevated Balls: "+str(num_starting_balls_slider.value))
-scene.append_to_caption('\n\n')
+#num_starting_balls_slider_caption = wtext(text = "Set Total # of Elevated Balls:")
+#num_starting_balls_slider = slider(bind=setNumBallsStarting, min=1, max=10, value=1, step=1)
+#num_starting_balls_text = wtext(text = "# of Elevated Balls: "+str(num_starting_balls_slider.value))
+#scene.append_to_caption('\n\n')
 
 set_initial_values = button(bind=start, text="Start", disabled=False)
 scene.append_to_caption('\n\n')
 
 while !set_initial_values.disabled:
     rate(program_rate)
+
 #print(initial_angle, num_balls, num_starting_balls)
 
 offset=-ball_radius*num_balls
@@ -78,26 +79,96 @@ rods=[]
 pos_curves=[]
 vel_curves=[]
 
-for i in range(num_starting_balls):
-    balls.append(sphere(pos=vec(L * sin(initial_angle)+2*i*ball_radius+offset,axis_pos-L * cos(initial_angle),0), radius=ball_radius, color=color.red, texture=textures.metal))
-    rods.append(cylinder(pos=balls[i].pos, axis=vec(2*i*ball_radius+offset,axis_pos,0)-balls[i].pos, radius=0.03, color=color.white))
-for i in range(num_starting_balls,num_balls):
+for i in range(0,num_balls):
     balls.append(sphere(pos=vec(2*i*ball_radius+offset,axis_pos-L,0), radius=ball_radius, color=color.white, texture=textures.metal))
     rods.append(cylinder(pos=balls[i].pos, axis=vec(0,L,0), radius=0.03, color=color.white))
-
-g = 9.81
-for i in range(num_balls):
     balls[i].acc=0
     balls[i].vel=0
     balls[i].theta=0
-    balls[i].prev_vel=0
-    balls[i].prev_theta=0
     curr_color=(vector.random()+vec(1,1,1))/2
     pos_curves.append(gcurve(graph=pos_graph, color=curr_color, label="Ball "+str(i)))
     vel_curves.append(gcurve(graph=vel_graph, color=curr_color, label="Ball "+str(i)))
-    
-for i in range(num_starting_balls):
-    balls[i].theta=initial_angle
+
+def on_click(ball, mouse):
+    dist=sqrt((mouse.x-ball.pos.x)*(mouse.x-ball.pos.x)+(mouse.y-ball.pos.y)*(mouse.y-ball.pos.y)+(mouse.z-ball.pos.z)*(mouse.z-ball.pos.z))
+    return dist<=ball_radius
+
+click_drag_text = text(text='Click and Drag', pos=vec(-4,3,0), color=color.red, visible=True)
+
+drag=False
+def start_drag():
+    global drag
+    drag=True
+def stop_drag():
+    global drag
+    drag=False
+
+scene.bind('mousedown', start_drag)
+scene.bind('mouseup', stop_drag)
+
+mouse_down = scene.waitfor('mousedown')
+
+starting_ball=-1
+while (starting_ball==-1):
+    rate(program_rate)
+    for i in range(0,num_balls):
+        if (on_click(balls[i], mouse_down.pos)):
+            starting_ball=i
+            break
+
+while drag:
+    rate(100)
+#    print(scene.mouse.pos.x, scene.mouse.pos.y)
+    initial_angle=asin(max(-1, min(1, (scene.mouse.pos.x-mouse_down.pos.x)/L)))
+    if (initial_angle<0):
+        num_starting_balls=starting_ball+1
+        for i in range(num_starting_balls):
+            balls[i].pos=pos=vec(L * sin(initial_angle)+2*i*ball_radius+offset,axis_pos-L * cos(initial_angle),0)
+            rods[i].pos=balls[i].pos
+            rods[i].axis=vec(2*i*ball_radius+offset,axis_pos,0)-balls[i].pos
+            balls[i].theta=initial_angle
+        for i in range(starting_ball, num_balls):
+            balls[i].pos=vec(2*i*ball_radius+offset, axis_pos-L,0)
+            rods[i].pos=balls[i].pos
+            rods[i].axis=vec(2*i*ball_radius+offset,axis_pos,0)-balls[i].pos
+            balls[i].theta=0
+    else:
+        num_starting_balls=num_balls-starting_ball
+        for i in range(starting_ball, num_balls):
+            balls[i].pos=vec(L * sin(initial_angle)+2*i*ball_radius+offset,axis_pos-L * cos(initial_angle),0)
+            rods[i].pos=balls[i].pos
+            rods[i].axis=vec(2*i*ball_radius+offset,axis_pos,0)-balls[i].pos
+            balls[i].theta=initial_angle
+        for i in range(0, starting_ball):
+            balls[i].pos=vec(2*i*ball_radius+offset, axis_pos-L,0)
+            rods[i].pos=balls[i].pos
+            rods[i].axis=vec(2*i*ball_radius+offset,axis_pos,0)-balls[i].pos
+            balls[i].theta=0
+#    
+#mouse_up = scene.waitfor('mouseup')
+click_drag_text.visible=False
+
+#initial_angle=atan((mouse_up.pos.x-balls[starting_ball].pos.x)/(mouse_up.pos.y-balls[starting_ball].pos.y))
+#
+#if (initial_angle<0):
+#    num_starting_balls=starting_ball+1
+#    for i in range(num_starting_balls):
+#        balls[i].pos=pos=vec(L * sin(initial_angle)+2*i*ball_radius+offset,axis_pos-L * cos(initial_angle),0)
+#        rods[i].pos=balls[i].pos
+#        rods[i].axis=vec(2*i*ball_radius+offset,axis_pos,0)-balls[i].pos
+#        balls[i].theta=initial_angle
+#else:
+#    num_starting_balls=num_balls-starting_ball
+#    for i in range(starting_ball, num_balls):
+#        balls[i].pos=pos=vec(L * sin(initial_angle)+2*i*ball_radius+offset,axis_pos-L * cos(initial_angle),0)
+#        rods[i].pos=balls[i].pos
+#        rods[i].axis=vec(2*i*ball_radius+offset,axis_pos,0)-balls[i].pos
+#        balls[i].theta=initial_angle
+
+#while True:
+#    rate(program_rate)
+
+g = 9.81
 t = 0
 dt = 1/2000
 
